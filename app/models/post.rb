e0ef0912b607c1146de99d978bae0d3a3fb76488
luisarita <github@luisarita.com>
2017-07-25 10:59:58 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, format: { with: /[a-z\s]*(won\'t believe|guess|secret|top [0-9]+)[a-z\s]*/i }
+    validates :content, length: { :minimum => 100 }
+    validates :summary, length: { :maximum => 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
